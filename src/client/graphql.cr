be9dc4d96a18427922d96dc142a196cf7194e404
hahwul <hahwul@gmail.com>
2023-05-01 15:32:56 +0900

    Add send_query

--- src/client/graphql.cr
+++ src/client/graphql.cr
@@ -2,8 +2,13 @@ require "crystal-gql"
 
 class CaidoClient
     @instance : GraphQLClient
-    
+
     def initialize(@endpoint : String)
         @instance = GraphQLClient.new endpoint
     end
+
+    def send_query(query : String, data : Hash(String, String))
+        @instance.instance.send_query query, data
+    end
 end
+
