3a80ec0ae4c729ba2ab4b203787dcf63ee73e328
HAHWUL <hahwul@gmail.com>
2024-02-26 00:26:01 +0900

    Linting

--- src/client/graphql.cr
+++ src/client/graphql.cr
@@ -1,23 +1,22 @@
 require "crystal-gql"
 
 class CaidoClient
-    @instance : GraphQLClient
+  @instance : GraphQLClient
 
-    # Initialize
-    def initialize(@endpoint : String)
-        @instance = GraphQLClient.new endpoint
-        if ENV.has_key? "CAIDO_AUTH_TOKEN"
-            @instance.headers["Authorization"] = "Bearer #{ENV["CAIDO_AUTH_TOKEN"]}"
-        end
+  # Initialize
+  def initialize(@endpoint : String)
+    @instance = GraphQLClient.new endpoint
+    if ENV.has_key? "CAIDO_AUTH_TOKEN"
+      @instance.headers["Authorization"] = "Bearer #{ENV["CAIDO_AUTH_TOKEN"]}"
     end
+  end
 
-    # Initialize with custom headers
-    def initialize(@endpoint : String, @headers : Hash(String, String))
-        @instance = GraphQLClient.new endpoint, headers
-    end
+  # Initialize with custom headers
+  def initialize(@endpoint : String, @headers : Hash(String, String))
+    @instance = GraphQLClient.new endpoint, headers
+  end
 
-    def query(query : String)
-        @instance.query query
-    end
+  def query(query : String)
+    @instance.query query
+  end
 end
-
