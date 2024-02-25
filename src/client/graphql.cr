df6c6ae514042a51b4a2fba1a4214a1ff13458e2
HAHWUL <hahwul@gmail.com>
2024-02-26 00:25:35 +0900

    Add authorization token to GraphQL client initialization

--- src/client/graphql.cr
+++ src/client/graphql.cr
@@ -3,8 +3,17 @@ require "crystal-gql"
 class CaidoClient
     @instance : GraphQLClient
 
+    # Initialize
     def initialize(@endpoint : String)
         @instance = GraphQLClient.new endpoint
+        if ENV.has_key? "CAIDO_AUTH_TOKEN"
+            @instance.headers["Authorization"] = "Bearer #{ENV["CAIDO_AUTH_TOKEN"]}"
+        end
+    end
+
+    # Initialize with custom headers
+    def initialize(@endpoint : String, @headers : Hash(String, String))
+        @instance = GraphQLClient.new endpoint, headers
     end
 
     def query(query : String)
