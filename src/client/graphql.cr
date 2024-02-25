5c19d893a649c1c97ce7096972a9553bccd4cadb
HAHWUL <hahwul@gmail.com>
2024-02-26 00:31:46 +0900

    Add headers to CaidoClient class

--- src/client/graphql.cr
+++ src/client/graphql.cr
@@ -2,18 +2,22 @@ require "crystal-gql"
 
 class CaidoClient
   @instance : GraphQLClient
+  @headers : Hash(String, String)
 
   # Initialize
   def initialize(@endpoint : String)
     @instance = GraphQLClient.new endpoint
+    @headers = Hash(String, String).new
+
     if ENV.has_key? "CAIDO_AUTH_TOKEN"
       @instance.headers["Authorization"] = "Bearer #{ENV["CAIDO_AUTH_TOKEN"]}"
+      @headers["Authorization"] = "Bearer #{ENV["CAIDO_AUTH_TOKEN"]}"
     end
   end
 
   # Initialize with custom headers
   def initialize(@endpoint : String, @headers : Hash(String, String))
-    @instance = GraphQLClient.new endpoint, headers
+    @instance = GraphQLClient.new endpoint, @headers
   end
 
   def query(query : String)
